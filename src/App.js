71a5cbde1025f393236a13b7f4593b1a19bfe696
eli9000 <eli9000@gmail.com>
2017-04-10 21:44:52 -0600

    youtube vid follow

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import uuid from 'uuid';
+import Header from './Components/Header';
+import AddProject from './Components/AddProject';
+import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects:  []
+    }
+  }
+
+  componentWillMount() {
+    this.setState({projects: [
+      {
+          id:uuid.v4(),
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          id:uuid.v4(),
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          id:uuid.v4(),
+          title: 'eCommerce Shopping Cart',
+          category: 'Web Development'
+        }
+    ]})
+  };
+
+  
+
+  handleAddProject(project) {
+   let projects = this.state.projects;
+   projects.push(project);
+   this.setState({projects: projects});
+  }
+
+  handleDeleteProject(id) {
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <AddProject addProject={this.handleAddProject.bind(this)} /><br />
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+        <div>
+            <h2>Header</h2>
+        </div>
+    );
+  }
+}
+
+export default Header;
